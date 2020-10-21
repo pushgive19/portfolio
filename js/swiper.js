682a2bef61f28d03902f7299f88898ae338a1593
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-10-21 09:15:33 +0900

    add content

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+
+var mySwiper = new Swiper('.main-swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.main-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.main-next',
+    prevEl: '.main-prev',
+  },
+
+})
