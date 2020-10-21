be4dda0dc8c0c5a082551474bf92fb6aff433a96
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-10-21 22:20:27 +0900

    web-slide

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,5 @@
 
-var mySwiper = new Swiper('.main-swiper', {
+var mainSwiper = new Swiper('.main-swiper', {
   // Optional parameters
   loop: true,
 
@@ -15,3 +15,38 @@ var mySwiper = new Swiper('.main-swiper', {
   },
 
 })
+
+
+var bgSwiper = new Swiper('.bg-swiper', {
+  // Optional parameters
+  loop: true,
+//  cssMode:{
+  //  simulateTouch:false,
+//  },
+  // If we need pagination
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.bg-next',
+    prevEl: '.bg-prev',
+  },
+
+})
+
+const mainBtnPrev = document.querySelector(".main-prev");
+const mainBtnNext = document.querySelector(".main-next");
+
+function clickPrev(){
+  setTimeout(function(){
+    bgSwiper.slidePrev(300,true);
+  }, 100)
+}
+function clickNext(){
+  setTimeout(function(){
+    bgSwiper.slideNext(300,true);
+  }, 100)
+}
+
+mainBtnPrev.addEventListener("click", clickPrev);
+mainBtnNext.addEventListener("click", clickNext);
+
